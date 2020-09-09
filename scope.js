536357c24848ebed4170317714d3beb9bd1ff2ec
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-09-09 16:54:03 +0600

    yey

---
+++ scope.js
@@ -0,0 +1,19 @@
+let bonus =20;
+
+function sum(first, second){
+    let result = first + second+ bonus;
+    console.log(result);
+    if(result>9){
+        const mood = "happy";
+        mood= "crazy";
+        mood = "cranky";
+        console.log(mood);
+    }
+    console.log(day);
+    let day= "friday";
+    return result;
+}
+
+// const output = sum(3,7);
+// console.log(bonus);
+// console.log(output);
